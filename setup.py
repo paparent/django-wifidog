a4d61d4d738a30ed91641ba48b0efa48ee6852db
PA Parent <paparent@paparent.me>
2012-07-27 11:54:00 -0400

    initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+
+setup(
+    name='wifidog',
+    version='0.0.1',
+    author='PA Parent',
+    author_email='paparent@paparent.me',
+    description='',
+    license='MIT',
+)
